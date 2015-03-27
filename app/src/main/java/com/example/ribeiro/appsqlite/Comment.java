23384a14e3a4662228cfe788cafeabed57cb9196
unknown <ribeiro@CQI-DEV-1997.employer.matriz>
2015-03-27 17:53:04 -0300

    Insert and delete methods OK

---
+++ app/src/main/java/com/example/ribeiro/appsqlite/Comment.java
@@ -0,0 +1,31 @@
+package com.example.ribeiro.appsqlite;
+
+/**
+ * Created by ribeiro on 27/03/2015.
+ */
+public class Comment {
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
